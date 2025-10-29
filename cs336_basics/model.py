881fa52f877a14ca0c0ea56b2f3aae7881a27add
Steven Chen <stevenshine@hotmail.co.uk>
2025-10-29 00:09:07 +0000

    refactor: Extract reset parameters into reset weight

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -3,6 +3,18 @@ import torch
 import torch.nn as nn
 from einops import einsum
 
+def reset_weight(weight: nn.Parameter) -> nn.Parameter:
+    d_out, d_in = weight.shape
+    variance = 2.0 / (d_in + d_out)
+    std = math.sqrt(variance)
+    return nn.init.trunc_normal_(
+        tensor=weight,
+        mean=0,
+        std=std,
+        a=-3*std,
+        b=3*std,
+    )
+
 class Linear(nn.Module):
     def __init__(self, in_features: int, out_features: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
         super().__init__()
@@ -13,14 +25,5 @@ class Linear(nn.Module):
         return einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
 
     def reset_parameters(self):
-        d_out, d_in = self.weight.shape
-        variance = 2.0 / (d_in + d_out)
-        std = math.sqrt(variance)
-        nn.init.trunc_normal_(
-            tensor=self.weight,
-            mean=0,
-            std=std,
-            a=-3*std,
-            b=3*std,
-        )
-    
+        reset_weight(self.weight)
+
