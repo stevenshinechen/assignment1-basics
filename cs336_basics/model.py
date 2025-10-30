7a6187926421d4b6a2b9912017b0fcaf4b14b6a2
Steven Chen <stevenshine@hotmail.co.uk>
2025-10-30 21:58:50 +0000

    feat: Add SwiGLU module

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -53,3 +53,16 @@ class RMSNorm(nn.Module):
         rms_norm = self.weight * (x / rms)
 
         return rms_norm.to(in_dtype)
+
+def silu(x: torch.Tensor) -> torch.Tensor:
+    return x * torch.sigmoid(x)
+
+class SwiGLU(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
+        super().__init__()
+        self.w1 = Linear(d_model, d_ff, device=device, dtype=dtype)
+        self.w2 = Linear(d_ff, d_model, device=device, dtype=dtype)
+        self.w3 = Linear(d_model, d_ff, device=device, dtype=dtype)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.w2(silu(self.w1(x)) * self.w3(x))
