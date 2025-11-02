479104ecb2095b769d83c0ed7d2f0dccfdc48d42
Steven Chen <stevenshine@hotmail.co.uk>
2025-11-02 23:13:04 +0000

    test: Add run_rope test adapter

--- tests/adapters.py
+++ tests/adapters.py
@@ -10,7 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 
-from cs336_basics.model import Embedding, Linear, RMSNorm, SwiGLU, scaled_dot_product_attention, softmax
+from cs336_basics.model import Embedding, Linear, RMSNorm, RotaryPositionalEmbedding, SwiGLU, scaled_dot_product_attention, softmax
 
 
 def run_linear(
@@ -208,7 +208,8 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = RotaryPositionalEmbedding(theta, d_k, max_seq_len)
+    return rope(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
