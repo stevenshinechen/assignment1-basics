c182712ac15b7e0c4d27550715b0dbe2e5a6b2db
Steven Chen <stevenshine@hotmail.co.uk>
2025-11-08 15:41:31 +0000

    feat: Add AdamW optimizer

--- cs336_basics/optimizer.py
+++ cs336_basics/optimizer.py
@@ -24,3 +24,51 @@ class SGD(torch.optim.Optimizer):
                 state["t"] = t + 1
 
         return loss
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(
+        self,
+        params: Iterable[torch.Tensor | dict],
+        lr: float = 0.001,
+        betas: tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-8,
+        weight_decay: float = 0.01,
+    ):
+        defaults = dict(
+            lr=lr,
+            betas=betas,
+            eps=eps,
+            weight_decay=weight_decay,
+        )
+        super().__init__(params, defaults)
+    
+    def step(self, closure: Callable[[], float] | None = None) -> float | None:
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            beta1, beta2 = group["betas"]
+            eps = group["eps"]
+            weight_decay = group["weight_decay"]
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                    
+                state = self.state[p]
+                if len(state) == 0:
+                    state["t"] = 1
+                    state["m"] = torch.zeros_like(p)
+                    state["v"] = torch.zeros_like(p)
+
+                grad = p.grad.data
+                state["m"] = beta1 * state["m"] + (1 - beta1) * grad
+                state["v"] = beta2 * state["v"] + (1 - beta2) * (grad**2)
+
+                t = state["t"]
+                lr_t = lr * (math.sqrt(1 - beta2**t) / (1 - beta1**t))
+                
+                p.data -= lr_t * state["m"] / (torch.sqrt(state["v"]) + eps)
+                p.data -= lr * weight_decay * p.data
+                state["t"] += 1
+
+        return loss
