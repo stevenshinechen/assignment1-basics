e1697afa7b1daac46abc499641d4dd96920d6c33
Steven Chen <stevenshine@hotmail.co.uk>
2025-11-08 18:18:23 +0000

    feat: Add get_lr_cosine_schedule

--- cs336_basics/optimizer.py
+++ cs336_basics/optimizer.py
@@ -72,3 +72,17 @@ class AdamW(torch.optim.Optimizer):
                 state["t"] += 1
 
         return loss
+
+def get_lr_cosine_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+) -> float:
+    if it < warmup_iters:
+        return (it / warmup_iters) * max_learning_rate
+    elif it <= cosine_cycle_iters:
+        return min_learning_rate + 0.5 * (1 + math.cos(math.pi * (it - warmup_iters) / (cosine_cycle_iters - warmup_iters))) * (max_learning_rate - min_learning_rate)
+    else:
+        return min_learning_rate
