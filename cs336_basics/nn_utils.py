285d9b676398040eae2b5da9c10b3a24c52845fd
Steven Chen <stevenshine@hotmail.co.uk>
2025-12-02 22:53:08 +0000

    feat: Add gradient clipping

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import torch
 from jaxtyping import Float, Int
 from torch import Tensor
@@ -16,3 +17,18 @@ def cross_entropy(
     log_probs = log_softmax(inputs, dim=-1)
     batch_indices = torch.arange(inputs.shape[0], device=inputs.device)
     return -log_probs[batch_indices, targets].mean()
+
+
+def gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float, eps: float = 1e-6) -> None:
+    g_norm = 0
+    for p in parameters:
+        if p.grad is not None:
+            g_norm += p.grad.data.norm(2)**2
+    
+    g_norm = g_norm.sqrt()
+
+    if g_norm >= max_l2_norm:
+        clip_coeff = max_l2_norm / (g_norm + eps)
+        for p in parameters:
+            if p.grad is not None:
+                p.grad *= clip_coeff
